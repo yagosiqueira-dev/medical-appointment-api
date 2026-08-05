c9b036b52dc974df8fe1073a359d8eecb2353065
yagosiqueira-dev <yagomachado847@gmail.com>
2026-08-04 22:19:12 -0300

    chore: initialize spring boot project with core dependencies

---
+++ src/main/java/com/yagosiqueira/medical_appointment_api/MedicalAppointmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.yagosiqueira.medical_appointment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppointmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yagosiqueira/medical_appointment_api/MedicalAppointmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagosiqueira.medical_appointment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
